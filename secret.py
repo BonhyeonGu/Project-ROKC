2597c34a74d32edf4d4b2e7b018420e1d1220435
whwhdtk <8whwhdtk8@naver.com>
2023-01-03 18:19:42 +0900

    도커설치 추가

---
+++ secret.py
@@ -0,0 +1,16 @@
+dbid = "neo4j"
+dbpw = "neo4j"
+dbaddr = "neo"
+dbport = "7687"
+
+neo_dbid = "neo4j"
+neo_dbpw = "neo4j"
+neo_dbaddr = "neo"
+neo_dbport = "7687"
+
+mongo_dbid = "root"
+mongo_dbpw = "g)2gH4G)4(3("
+mongo_dbaddr = "mongo"
+mongo_dbport = 27017
+
+session_secret_key = "1234"
